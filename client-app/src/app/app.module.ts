9affb2d609a56b7f6e4fd651839fb69cda0f4c66
Hendi Santika <hendisantika@gmail.com>
2018-02-17 09:03:34 +0700

    #16 - Add User Angular Component

--- client-app/src/app/app.module.ts
+++ client-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
