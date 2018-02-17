7f05006d2afd157e251526183e6c124f9c3ec7d1
Hendi Santika <hendisantika@gmail.com>
2018-02-17 09:12:56 +0700

    #21 - Modify app routing - Client App

--- client-app/src/app/core/app.routing.module.ts
+++ client-app/src/app/core/app.routing.module.ts
@@ -1,3 +1,8 @@
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from '../login/login.component';
+import {UserComponent} from '../user/user.component';
+import {NgModule} from '@angular/core';
+
 const routes: Routes = [{path: 'user', component: UserComponent}, {path: 'login', component: LoginComponent}, {
   path: '',
   component: LoginComponent
