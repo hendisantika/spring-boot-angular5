70c797d52aacce35b720500193fbd4f3a4683c60
Hendi Santika <hendisantika@gmail.com>
2018-02-17 08:20:58 +0700

    #1 - Initial Commit - Client App

---
+++ client-app/src/main/java/com/hendisantika/clientapp/ClientAppApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.clientapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientAppApplication.class, args);
+	}
+}
