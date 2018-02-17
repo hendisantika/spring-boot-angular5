f87723d72b7bad610e5c18011057fe42acbe1913
Hendi Santika <hendisantika@gmail.com>
2018-02-17 08:33:50 +0700

    #6 - Add User Repository - Backend App

---
+++ backend-app/src/main/java/com/hendisantika/clientapp/repository/UserRepository.java
@@ -0,0 +1,27 @@
+package com.hendisantika.clientapp.repository;
+
+import com.hendisantika.clientapp.entity.User;
+import org.springframework.data.repository.Repository;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : backend-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/02/18
+ * Time: 08.33
+ * To change this template use File | Settings | File Templates.
+ */
+public interface UserRepository extends Repository<User, Integer> {
+
+    void delete(User user);
+
+    List<User> findAll();
+
+    User findOne(int id);
+
+    User save(User user);
+}
