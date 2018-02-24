ea748f54f7b0d8262f1cefef5dd4b611b15b6fff
Hendi Santika <hendisantika@gmail.com>
2018-02-25 06:19:14 +0700

    #29 - Modify app.routing.module TS - Client App

--- client-app/src/app/core/app.routing.module.ts
+++ client-app/src/app/core/app.routing.module.ts
@@ -3,11 +3,18 @@ import {LoginComponent} from '../login/login.component';
 import {UserComponent} from '../user/user.component';
 import {NgModule} from '@angular/core';
 
-const routes: Routes = [{path: 'user', component: UserComponent}, {path: 'login', component: LoginComponent}, {
-  path: '',
-  component: LoginComponent
-}];
+const routes: Routes = [
+  {path: 'user', component: UserComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '', component: LoginComponent}
+];
 
-@NgModule({imports: [RouterModule.forRoot(routes)], exports: [RouterModule], declarations: []})
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)],
+  exports: [
+    RouterModule
+  ], declarations: []
+})
 export class AppRoutingModule {
 }
