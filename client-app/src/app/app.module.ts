5e5fbed1178504e0fea925cf91a112f6670f3179
Hendi Santika <hendisantika@gmail.com>
2018-04-08 07:56:48 +0700

    #35 - Delete core components & modify app.module.ts - Client App

--- client-app/src/app/app.module.ts
+++ client-app/src/app/app.module.ts
@@ -1,11 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { UserComponent } from './user/user.component';
-
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {UserComponent} from './user/user.component';
 
 @NgModule({
   declarations: [
--- client-app/src/app/core/material.module.ts
+++
@@ -1,8 +0,0 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {MatButtonModule, MatToolbarModule} from '@angular/material';
-
-@NgModule({imports: [CommonModule, MatToolbarModule, MatButtonModule], exports: [CommonModule, MatToolbarModule, MatButtonModule],})
-export class CustomMaterialModule {
-}
-
